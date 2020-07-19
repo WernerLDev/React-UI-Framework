816c948d898af3e711aea55d2477c5c2fa5b9a97
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2020-07-19 15:44:40 +0200

    Improve pane component

--- src/layouts/pane/pane.tsx
+++ src/layouts/pane/pane.tsx
@@ -3,11 +3,23 @@ import './pane.scss'
 
 export const Pane = (
     props: {
+        width?: number,
+        height?: number,
+        background?: string,
         children:React.ReactNode
     }
 ) => {
+    const style: React.CSSProperties = {
+        width: props.width ? `${props.width}px` : undefined,
+        height: props.height ? `${props.height}px` : undefined,
+        minWidth: props.width ? `${props.width}px` : undefined,
+        minHeight: props.height ? `${props.height}px` : undefined,
+        flexGrow: props.width == null && props.height == null ? 1 : undefined,
+        alignSelf: props.width == null && props.height == null ? 'stretch' : undefined,
+        background: props.background
+    }
     return (
-        <div className="pane">
+        <div style={style} className="pane">
             {props.children}    
         </div>
     )

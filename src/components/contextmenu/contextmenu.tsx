721ee68647e939b55bb32aa7b180c90bb6a4e100
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2020-10-11 18:26:40 +0200

    Improve contextmenu placement

--- src/components/contextmenu/contextmenu.tsx
+++ src/components/contextmenu/contextmenu.tsx
@@ -10,12 +10,19 @@ export interface IContextMenuProps {
 
 export const ContextMenu = ({items, position, onClose}: IContextMenuProps) => {
 
+    const offsetHeight = document.body.offsetHeight;
+    const offsetWidth = document.body.offsetWidth;
+    const isAtBottom = position.top > offsetHeight / 2;
+    const isAtRight = offsetWidth - position.left < 300;
+
     return (
         <div
             style={{
                 position: "fixed",
-                top: `${position.top}px`,
-                left: `${position.left}px`
+                top: isAtBottom ? undefined : `${position.top}px`,
+                left: isAtRight ? undefined : `${position.left}px`,
+                bottom: isAtBottom ? `${offsetHeight - position.top}px` : undefined,
+                right: isAtRight ? `${offsetWidth - position.left}px` : undefined,
             }}
         >
             <Menu 
